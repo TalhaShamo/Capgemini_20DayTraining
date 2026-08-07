d2bc0cd0c98189fae113bba2d96fcfa6e814214e
TalhaShamo <talha246135@gmail.com>
2026-08-07 15:00:10 +0530

    DailyQ

---
+++ D9_Topbrains22/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static int CountPaths(int rows, int cols)
+    {
+        if (rows == 1 || cols == 1)
+            return 1;
+
+        return CountPaths(rows - 1, cols) + CountPaths(rows, cols - 1);
+    }
+
+    static void Main()
+    {
+        Console.WriteLine(CountPaths(3, 3));
+    }
+}
